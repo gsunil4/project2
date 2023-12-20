33e49799d37dabd63550b221dee5206bcb0068c7
Gsunil <gsunilgouda172000@gmail.com>
2023-12-20 16:52:29 +0530

    second

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express")
 const app = express()
 const formidable = require("express-formidable")
 const mongoose = require("mongoose")
-const port = 3000
-app.listen(port, console.log("server running"))
-// require("./batabase/conn")
+let customer = require('./customer.js')
+const bcrypt = require('bcrypt');
+
+
+
+
 
 mongoose.connect("mongodb://0.0.0.0:27017/student")
     .then(() => console.log("database is connected"))
@@ -18,19 +21,30 @@ const userdata =new mongoose.Schema({
 })
 const reg= mongoose.model("empdata", userdata)
 
-app.post("/register", formidable(), function (req, res) {
+app.post("/register", formidable(), async function (req, res) {
     let { firstName, lastName ,email ,password} =req.fields
 
     if (! (firstName && lastName && email && password)){
         res.status(400).send("check info u provided")
     }
-    else {
-      const data= reg.create({
-        firstName: req.fields.firstName,
-        lastName: req.fields.lastName,
-        email: req.fields.email,
-        password: req.fields.password,
-        })
-        res.status(200).send("database created")
+    else { 
+        if (await reg.findOne({email})){
+            res.send('you have already registered')
+        }
+        else {
+            let enc_password = await bcrypt.hash(password, 9)
+            const data = await reg.create({
+                firstName: req.fields.firstName,
+                lastName: req.fields.lastName,
+                email: req.fields.email,
+                password: enc_password,
+            })
+            res.json(data)
+            //res.status(200).send("database created")
+        }
     }
 })
+
+
+
+app.listen(3000,()=>{console.log('connected on 3000 Port')})
